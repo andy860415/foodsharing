870a632309e0dd0fce037e4ff94cdcbbfec8e1c3
yangyang95 <yangyang.weng@gmail.com>
2017-06-07 17:37:47 +0800

    Support image upload
    Submit food info and image with form, send image up to server
    then upload to imgur. After that redirect to preview page.

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 
+// modules - make sure you install all the module
 const express = require('express')
 const bodyParser = require('body-parser')
-const fs = require('fs')
+const fs = require('fs-extra')
+const formidable = require('formidable')
+
+// custom function
 const user = require('./njs_exports/user')
 const trade = require('./njs_exports/trade')
 const foodpage = require('./njs_exports/foodpage')
 const map = require('./njs_exports/map')
 const data = require('./njs_exports/data')
+const edit = require('./njs_exports/edit')
+const imgur = require('./njs_exports/upload_imgur')
+
 const app = express()
-
-
-
-
-
-var edit = require('./njs_exports/edit')
-
-
 
 //set the viewegin fiele path
 app.set('views', __dirname + '/views')
@@ -70,9 +69,50 @@ app.post('/doRegister',(req,res) => {
 app.get('/ajax',(req,res) =>{
 	res.send('hi'+req.query.name)
 })
-app.post('/doPreview',(req,res) => {
-	edit.doPreview(req,res)
-})
+
+
+// Upload image & preview
+app.post('/upload',  function(req, res) {
+  var post_data = {};
+
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    // Store post data
+    post_data.logo_text = fields.logo_text;
+    post_data.food = fields.food;
+    post_data.price = fields.price;
+    post_data.number = fields.number;
+    post_data.place = fields.place;
+    post_data.time = fields.time;
+    post_data.gettime = fields.gettime;
+    post_data.suggest = fields.suggest;
+  });
+
+  form.on('end', function(fields, files) {
+    /* Temporary location of the upload file */
+    var temp_path = this.openedFiles[1].path;
+    /* The file name of the upload file */
+    var file_name = this.openedFiles[1].name;
+    /* Location where we put the uploaded file */
+    var new_location = 'uploads/';
+
+    // copy image to server
+    fs.copy(temp_path, new_location + file_name, function(err) {
+			if (err) {
+        console.log(err);
+      } else {
+        // upload to imgur
+        imgur.setup(req, res);
+        imgur.upload(req, res, new_location + file_name, (link) => {
+          post_data.pic_path = link;
+          // preview post
+          edit.doPreview(req, res, post_data, link);
+        });
+      }
+    });
+  });
+});
+
 
 
 var server = app.listen(3333, function() {
--- njs_exports/edit.js
+++ njs_exports/edit.js
@@ -1,14 +1,14 @@
-const doPreview = function (req, res) {
+const doPreview = function (req, res, post_data, link) {
   res.render('preview',{
-    logo_text: req.body['logo_text'],
-    pic_path: req.body['pic_path'],
-    food: req.body['food'],
-		price: req.body['price'],
-		number: req.body['number'],
-    place: req.body['place'],
-		time: req.body['time'],
-    gettime: req.body['gettime'],
-		suggest: req.body['suggest']
+    logo_text: post_data.logo_text,
+    pic_path: link,
+    food: post_data.food,
+		price: post_data.price,
+		number: post_data.number,
+    place: post_data.place,
+		time: post_data.time,
+    gettime: post_data.gettime,
+		suggest: post_data.suggest
   })
 }
 
